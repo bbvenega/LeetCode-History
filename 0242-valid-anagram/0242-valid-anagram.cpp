3b53f7fd737124a3e1e23ae7880c7fe0208b0f4e
Brian Venegas <146390843+bbvenega@users.noreply.github.com>
2024-06-03 21:32:16 -0700

    Time: 6 ms (80.18%), Space: 8.5 MB (61.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+                if(s.length() != t.length()) {
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            tolower(s[i]);
+            tolower(t[i]);
+        }
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+
+        
+
+        if(s == t) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
