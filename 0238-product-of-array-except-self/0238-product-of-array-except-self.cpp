5343c18b656fb101f3e944f9e18494ad85610b58
Brian Venegas <146390843+bbvenega@users.noreply.github.com>
2024-08-16 13:19:53 -0700

    Time: 25 ms (71.14%), Space: 38.4 MB (74.98%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        vector<int> ans;
-        int product = 1;
-        int zeroCounter = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            
-            if(nums[i] != 0) {
-                 product = product * nums[i];  
-            }   else {
-                zeroCounter++;
-            }
-    }
-
-    // cout << "PRoduct is: " << product << endl;
-        for(int i = 0; i < nums.size(); i++) {
-
-            if(zeroCounter > 1) {
-                ans.push_back(0);
-            }
-            if(zeroCounter <= 0) {
-                ans.push_back(product / nums[i]);
-            } if(zeroCounter == 1) {
-                    if(nums[i] == 0) {
-                // cout << "Pushing: " << product << endl;
-            ans.push_back(product);
-            } else {
-                ans.push_back(0);
-                }
-            }
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefix;
+            prefix = prefix * nums[i];
         }
 
+        int postfix = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * postfix;
+            postfix = postfix * nums[i];
+        }
         return ans;
     }
 };
